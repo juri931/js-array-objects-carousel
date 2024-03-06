7ac893a70a4fd2f3656a7b32789b2f32511aa57b
juri931 <impa.mat@hotmail.it>
2024-03-06 15:30:42 +0100

    [fix] some fixes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let intervalId = null;
 
-document.addEventListener("DOMContentLoaded", initializeSlider);
+const imgText = document.querySelector("span")
+
+initializeSlider();
 
 function initializeSlider(){
   if (slides.length > 0) {
@@ -20,8 +22,11 @@ function showSlide(index){
 
   slides.forEach(slide => {
   slide.classList.remove("displaySlide");
-  });
-  slides[slideIndex].classList.add("displaySlide");
+  
+});
+slides[slideIndex].classList.add("displaySlide");
+imgText.classList.add("position-absolute");
+
 }
 
 function prevSlide(){
